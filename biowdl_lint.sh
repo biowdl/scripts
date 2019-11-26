af1a07fb6783e24464b4d59c7827bd4e898302a2
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2019-11-26 16:38:05 +0100

    add linting code

---
+++ biowdl_lint.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+for WDL_FILE in $(git ls-files *.wdl)
+  do
+    echo $WDL_FILE
+    womtool validate $WDL_FILE
+  done
+git submodule foreach --recursive \
+bash -c '\
+if ["$(git tag --contais)" == "" ]; \
+  then git checkout develop && git pull && \
+  git submodule update --init --recursive ; \
+  else echo "on tag: $(git tag --contains)" ; \
+fi
+'
+git diff --exit-code || \
+(echo ERROR: Git changes detected. Submodules should either be tagged or on the latest version of develop. && exit 1)
